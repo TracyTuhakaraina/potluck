63d1bd029a885d294c5acef76ab799e28f3a8b4b
Tracy Tuhakaraina <tracytmail@gmail.com>
2024-11-26 20:27:17 +1100

    Assigned potluck dishes

--- js/script.js
+++ js/script.js
@@ -11,9 +11,15 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+const assignButton = document.querySelector(".assign");
+
+const assignedItems = document.querySelector(".assigned-items");
+
+
 addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
     //console.log(guest);
+
     if (guest !== "") {
         addToList(guest);
         updateGuestCount();
@@ -22,14 +28,14 @@ addGuestButton.addEventListener("click", function () {
     }
 });
 
-const clearInput = function () {
-    guestInput.value = "";
-};
-
 const addToList = function (guest) {
     const listItem = document.createElement("li");
     listItem.innerText = guest;
     guestList.append(listItem);
+};
+
+const clearInput = function () {
+    guestInput.value = "";
 };
 
 const updateGuestCount = function () {
@@ -43,3 +49,33 @@ const updateGuestCount = function () {
         guestFull.classList.remove("hide");
     }
 };
+
+const assignItems = function () {
+    const potluckItems = [
+        "potato salad",
+        "cheese board with crackers",
+        "corn dip with cornchips",
+        "creamy bruschetta dip with bread",
+        "vegetable platter",
+        "green salad",
+        "fruit salad",
+        "jamaican rum punch",
+    ];
+
+    const allGuests = document.querySelectorAll(".guest-list li");
+
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+        assignedItems.append(listItem);
+        potluckItems.splice(randomPotluckIndex, 1);
+    }
+};
+
+assignButton.addEventListener("click", function () {
+    assignItems();
+    assignButton.disabled = true;
+});
+
